dbc8efc8a6b07d01dc9a3d684ae3d6391bee27ea
tommn1207 <khoinguyennguyen85@gmail.com>
2026-06-14 18:48:52 +0700

    Fix iOS PWA recognition: Add scope to manifest and apple-icon.jpg

--- app/manifest.js
+++ app/manifest.js
@@ -4,7 +4,9 @@ export default function manifest() {
     short_name: 'EFY Studio',
     description: 'Premium Pilates Studio in Berlin Lichterfelde West.',
     start_url: '/',
+    scope: '/',
     display: 'standalone',
+    background_color: '#1e1812',
     icons: [
       {
         src: '/icon.svg',
